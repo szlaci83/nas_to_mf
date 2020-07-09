781da192ed6cbef78855c367cac80e59221070d5
laci <szlaci83@gmail.com>
2020-07-09 14:47:46 +0100

    getting properties from env vars

--- properties_example.py
+++ properties_example.py
@@ -1,12 +1,19 @@
+import os
+
 # MediaFire
-email = ''
-password = ''
-app_id = ''
+email = os.environ.get('MF_EMAIL')
+password = os.environ.get('MF_PASSWORD')
+app_id = os.environ.get('MF_APP_ID')
 DONE_FILE = ''
 
 # FTP
-private_ftp = ''
-public_ftp = ''
+private_ftp = os.environ.get('LOCAL_FTP')
+public_ftp = os.environ.get('PUBLIC_FTP')
 ftp_host = public_ftp
-ftp_user = ''
-ftp_password = ''
+ftp_user = os.environ.get('FTP_USER')
+ftp_password = os.environ.get('FTP_PASSWORD')
+
+
+if __name__ == '__main__':
+    from pprint import pprint as p
+    p(globals())
